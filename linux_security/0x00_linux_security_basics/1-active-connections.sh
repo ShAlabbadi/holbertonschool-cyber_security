bc418b68618a50f913240c8afc0ec69cd781ae85
ShAlabbadi <10708@holbertonstudents.com>
2026-02-25 15:04:09 -0500

    1. Shows your Linux connections, not your social status

--- linux_security/0x00_linux_security_basics/1-active-connections.sh
+++ linux_security/0x00_linux_security_basics/1-active-connections.sh
@@ -1,8 +1,2 @@
-# sudo: run as sudoers.
-# ss: displays detailed information about network sockets
-# -a: All sockets (listening + established)
-# -n: Numerical addresses
-# -t: TCP sockets only
-# -p: Show process information
 #!/bin/bash
 sudo ss -tanp
